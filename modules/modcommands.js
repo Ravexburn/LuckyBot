01177c2fc023d3b44b324de8510b89abc2c26496
Rave <asgarber@live.com>
2018-01-04 20:00:10 -0500

    code clean up and moved files

---
+++ modules/modcommands.js
@@ -0,0 +1,323 @@
+const Discord = require("discord.js");
+const invite = "";
+const Message = Discord.Message;
+
+module.exports = (bot = Discord.Client) => {
+
+    require("./../functions/modchanfunctions.js")(bot);
+    require("./../functions/modcmdfunctions.js")(bot);
+    require("./../functions/modtogfunctions.js")(bot);
+
+    //Admin and Mod Settings
+
+    bot.on("message", async message => {
+        if (message.author.bot) return;
+        if (message.channel.type === "dm") return;
+
+        let perms = ["ADMINISTRATOR", "MANAGE_GUILD", "VIEW_AUDIT_LOG"];
+        let allowed = false;
+
+        for (i = 0; i < perms.length; i++) {
+            if (message.guild.member(message.author).hasPermission(perms[i])) allowed = true;
+        }
+        if (!allowed) return;
+
+        let serverSettings = bot.getServerSettings(message.guild.id);
+
+        let messageArray = message.content.split(" ");
+        let command = messageArray[0];
+        let args = messageArray.slice(1);
+        const prefix = serverSettings.prefix;
+
+        if (!command.startsWith(prefix)) return;
+
+        //Prefix
+
+        if ((command === `${prefix}setprefix`)) {
+            setPrefix(message, command, args, serverSettings);
+        }
+
+        //Starting logs, roles, join, and music
+
+        if ((command === `${prefix}start`)) {
+            if (args.length === 0) {
+                message.channel.send(`\`\`\`md\nTo use start please use one of the following subcommands: \n${command} <help|messagelogs|logs|roles|music>\`\`\``);
+                return;
+            }
+            switch (args[0].toLowerCase()) {
+
+                //Help
+
+                case "help":
+
+                    message.channel.send("<:monkaS:372547459840475146> h-help");
+                    break;
+
+                //Message logging
+
+                case "logs":
+                case "messagelogs":
+
+                    logChan(message);
+                    break;
+
+                //Roles
+
+                case "roles":
+
+                    rolesChan(message, serverSettings, command);
+                    break;
+
+                //Welcome
+
+                case "welcome":
+
+                    message.channel.send(`Please use *welcome channel <#channelname> to set a welcome channel`);
+                    break;
+
+                //Join
+
+                case "join":
+
+                    joinChan(message, serverSettings, command);
+                    break;
+
+                //Music to be added at a later date
+
+                /*  case "music":
+ 
+                 musicChan(message, serverSettings, command);
+                     break; */
+
+                //Edited messages
+
+                case "edit":
+
+                    editChan(message, serverSettings, command, args);
+                    break;
+
+                //Deleted messages
+
+                case "delete":
+
+                    delChan(message, serverSettings, command, args);
+                    break;
+
+                default:
+
+                    message.channel.send(`\`\`\`md\nTo use start please use one of the following subcommands: \n${command} <help|messagelogs|logs|roles|music>\`\`\``);
+                    break;
+            }
+        }
+
+        //Toggles
+
+        if ((command === `${prefix}toggle`)) {
+            if (args.length === 0) {
+                message.channel.send(`\`\`\`md\nTo use toggle please use one of the following subcommands: \n${command} <image|logs>\`\`\``);
+                return;
+            }
+            let emote = "";
+            switch (args[0].toLowerCase()) {
+
+                //Toggle Image Embed
+                case "image":
+
+                    imgTog(message, serverSettings);
+                    return;
+
+                //Toggle Logs
+                case "logs":
+
+                    logsTog(message, serverSettings);
+                    return;
+
+                default:
+                    return;
+            }
+        }
+
+        //Welcome setup
+        //*welcome <help|channel|message>
+        //*welcome channel in #channel
+        //*welcome channel #channel
+
+        if ((command === `${prefix}welcome`)) {
+            if (args.length === 0) {
+                message.channel.send(`\`\`\`md\nTo use welcome please use one of the following subcommands: \n${command} <help|channel|message>\`\`\``);
+                return;
+            }
+            switch (args[0].toLowerCase()) {
+
+                case "help":
+
+                    message.channel.send("<:monkaS:372547459840475146> h-help");
+                    break;
+
+                case "chan":    
+                case "channel":
+
+                    welChan(message, serverSettings, command);
+                    break;
+
+                case "msg":
+                case "message":
+
+                    welMsg(message, command, args, serverSettings);
+                    break;
+
+                default:
+
+                    message.channel.send(`\`\`\`md\nTo use welcome please use one of the following subcommands: \n${command} <help|channel|message>\`\`\``);
+                    break;
+            }
+        }
+
+        
+        //Ban Command
+
+        if ((command === `${prefix}ban`)) {
+            banUser(message, command, args, perms);
+        }
+
+        //Kick command
+
+        if ((command === `${prefix}kick`)) {
+            kickUser(message, command, args, perms);
+        }
+    });
+
+    //Intial Settings (Owner Only)
+
+    bot.on("message", async message => {
+        if (message.author.bot) return;
+        if (message.channel.type === "dm") return;
+        if (![bot.botSettings.Owner_id, bot.botSettings.Owner_id2].includes(message.author.id)) return;
+
+        let messageArray = message.content.split(" ");
+        let command = messageArray[0];
+        let args = messageArray.slice(1);
+        let prefix = bot.botSettings.prefix;
+        if (!command.startsWith(prefix)) return;
+
+        //Default Settings
+
+        if ((command === `${prefix}intset`)) {
+
+            bot.initServerSettings(message.guild.id);
+            message.channel.send("**Server settings have been reset**")
+                .then(message => message.delete(10 * 1000));
+            message.delete(10 * 1000);
+            return;
+        }
+
+        //Servers' info
+        //Server <help|list|leave>
+
+        if ((command === `${prefix}server`) || (command === `${prefix}getmeout`)) {
+            if (args.length === 0) {
+                message.channel.send(`\`\`\`md\nTo use server please use one of the following subcommands: \n${command} <help|list|leave>\`\`\``);
+                return;
+            }
+            switch (args[0].toLowerCase()) {
+
+                case "help":
+                    message.channel.send("<:monkaS:372547459840475146> h-help");
+                    break;
+
+                case "list":
+                    let msg = "```md\n"
+                    let i = 1;
+                    bot.guilds.forEach(guild => {
+                        msg += `${i++}. ${guild.name} - <${guild.id}>\n`;
+                    });
+                    msg += "```";
+                    message.channel.send(msg);
+                    break;
+
+                case "leave":
+                    if (args.length < 2) {
+                        message.channel.send("Put a server id or server number <:yfist:378373231079587840>");
+                        let msg = "```md\n"
+                        let i = 1;
+                        let guilds = [];
+                        bot.guilds.forEach(guild => {
+                            guilds[i] = guild;
+                            msg += `${i++}. ${guild.name} - <${guild.id}>\n`;
+                        });
+                        msg += "```";
+                        message.channel.send(msg);
+                        let author = message.author;
+                        message.channel.send("You have 60 seconds to choose a server to leave")
+                            .then(() => {
+                                message.channel.awaitMessages(response => response.author.id === author.id, { max: 1, time: 60000, errors: ['time'] })
+                                    .then(collected => {
+                                        message.channel.send(`Acknowledged ${collected.first().content}`);
+                                        let input = collected.first().content;
+
+                                        if (input.length < 18) {
+                                            let num = parseInt(input);
+
+                                            if (num === NaN) {
+                                                message.channel.send("Not a valid server <:yfist:378373231079587840>");
+                                                return;
+                                            }
+
+
+                                            if ((num <= 0) || (num > guilds.length)) {
+                                                message.channel.send("Not a valid server <:yfist:378373231079587840>");
+                                                return;
+                                            }
+
+                                            message.channel.send(`Successfully left \`${guilds[num]}\``);
+                                            guilds[num].leave().catch(console.error);
+
+                                        } else {
+
+                                            let id = input;
+                                            if (!bot.guilds.has(id)) {
+                                                message.channel.send("Bot is not in that server <:yfist:378373231079587840>");
+                                                return;
+                                            }
+
+                                            let guild = bot.guilds.get(id);
+                                            message.channel.send(`Successfully left \`${guild}\``);
+                                            guild.leave().catch(console.error);
+                                        }
+
+                                    })
+
+                                    .catch(() => {
+                                        message.channel.send("No server sent in time limit");
+                                        console.error;
+                                    });
+
+                            }).catch(() => console.error);
+                        return;
+                    }
+                    if (args[1].length > bot.guilds.size.toString().length) {
+
+
+                        let id = args[1];
+                        if (!bot.guilds.has(id)) {
+                            message.channel.send("Bot is not in that server <:yfist:378373231079587840>");
+                            return;
+                        }
+
+                        let guild = bot.guilds.get(id);
+                        message.channel.send(`Successfully left \`${guild}\``);
+                        guild.leave().catch(console.error);
+
+                    }
+                    break;
+
+                default:
+                    message.channel.send(`\`\`\`md\nTo use server please use one of the following subcommands: \n${command} <help|list|leave>\`\`\``);
+                    break;
+            }
+            return;
+        }
+
+    });
+
+}
---
+++ modules/roles.js
@@ -0,0 +1,164 @@
+const Discord = require("discord.js");
+const GuildMember = Discord.GuildMember;
+const Role = Discord.Role;
+
+
+module.exports = (bot = Discord.Client) => {
+
+    bot.on("message", async message => {
+        if (message.author.bot) return;
+        if (message.channel.type === "dm") return;
+        const serverSettings = bot.getServerSettings(message.guild.id);
+        if (!serverSettings) return;
+
+        let prefix = serverSettings.prefix;
+        let valid_channel = false;
+        let activeChannel;
+
+        if (message.channel.id === serverSettings.roleChannelID) {
+            valid_channel = true;
+        }
+
+        if (message.guild.channels.has(serverSettings.roleChannelID)) {
+            activeChannel = message.guild.channels.get(serverSettings.roleChannelID);
+        }
+
+        if (!activeChannel) return;
+
+        let msgAry = message.content.trim().split(" ");
+
+        switch (msgAry[0].charAt(0)) {
+            case "+":
+            case "-":
+                break;
+            default:
+                return;
+        }
+
+        let length = msgAry.length;
+        let command = "";
+        let rolename = "";
+        for (let i = 0; i < length; i++) {
+            let arg = msgAry[i];
+            switch (arg.charAt(0)) {
+                case "+":
+                case "-":
+
+                    command = arg.charAt(0);
+                    arg = arg.slice(1);
+
+                default:
+                    if (rolename !== "") {
+                        rolename += " ";
+                    }
+                    rolename += arg;
+                    break;
+            }
+
+            if ((i + 1) < length) {
+
+                if ((msgAry[i + 1].charAt(0) !== "+") && (msgAry[i + 1].charAt(0) !== "-")) {
+                    continue;
+                }
+
+            }
+
+            if (command !== "") {
+                switch (command) {
+                    case "+":
+                        addRole(message.member, rolename, message);
+                        break;
+                    case "-":
+                        removeRole(message.member, rolename, message);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            command = "";
+            rolename = "";
+        }
+
+    });
+
+}
+
+function addRole(member, rolename, message) {
+
+    if (!member) return false;
+    if (!rolename) return false;
+
+    if (member.guild.roles.exists("name", rolename)) {
+        let role = member.guild.roles.find("name", rolename);
+
+        if (member.roles.has(role.id)) {
+            //Member has role
+            message.reply("You already have this role.")
+                .then(message => message.delete(10 * 1000));
+            message.delete(10 * 1000);
+            return false;
+        }
+
+        if (canAddRole(member, role)) {
+            member.addRole(role).catch(console.error);
+            message.reply("Role has been added.")
+                .then(message => message.delete(10 * 1000));
+            message.delete(10 * 1000);
+            return true;
+        }
+        else {
+            message.reply("You do not have the required permissions to add that role.")
+                .then(message => message.delete(10 * 1000));
+            message.delete(10 * 1000);
+            return false;
+        }
+
+    }
+    return false;
+}
+
+function removeRole(member, rolename, message) {
+
+    if (!member) return false;
+    if (!rolename) return false;
+
+    if (member.guild.roles.exists("name", rolename)) {
+        let role = member.guild.roles.find("name", rolename);
+        if (member.roles.has(role.id)) {
+            //Member has role
+            return member.removeRole(role).then(() => {
+                message.reply("Role has been removed.")
+                    .then(message => message.delete(10 * 1000));
+                message.delete(10 * 1000);
+                return true;
+            }).catch(() => {
+                message.reply("Failed to remove role.").then(message => message.delete(10 * 1000));
+                message.delete(10 * 1000);
+            });
+
+        }
+        else {
+            message.reply("You do not have that role.")
+                .then(message => message.delete(10 * 1000));
+            message.delete(10 * 1000);
+            return false;
+        }
+    }
+}
+
+/**
+ * Checks if role can be added to user.
+ * @param {GuildMember} member 
+ * @param {Role} role 
+ * @returns {boolean}
+ */
+function canAddRole(member, role) {
+    let perms = ["ADMINISTRATOR", "MANAGE_GUILD", "KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_CHANNELS", "VIEW_AUDIT_LOG", "MENTION_EVERYONE", "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_EMOJIS"];
+    for (i = 0; i < perms.length; i++) {
+        if (role.hasPermission(perms[i])) {
+            return false;
+        }
+    }
+    return true;
+}
