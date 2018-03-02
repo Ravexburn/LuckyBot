91d8343c5777e9ccfd3006a7d075fbc21f0cfd86
Rave <asgarber@live.com>
2018-03-02 02:42:54 -0500

    updates

--- modules/lastfm.js
+++ modules/lastfm.js
@@ -41,13 +41,20 @@ module.exports = (bot = Discord.Client) => {
                             message.reply(response.message);
                             return Promise.reject(response.message);
                         }
+                        if (!response.data.user) {
+                            console.log(`Blank Account`);
+                            return;
+                        }
                         let thumbnailURL = "";
-                        response.data.user.image.forEach(image => {
-                            if (image["size"] === "extralarge") {
-                                thumbnailURL = image["#text"];
-                            }
-
-                        })
+                        if (!response.data.user.image) {
+                            console.log(`No image found`);
+                        } else {
+                            response.data.user.image.forEach(image => {
+                                if (image["size"] === "extralarge") {
+                                    thumbnailURL = image["#text"];
+                                }
+                            });
+                        }
                         let date = new Date(response.data.user.registered.unixtime * 1000);
 
                         let embed = new Discord.RichEmbed()
@@ -63,15 +70,14 @@ module.exports = (bot = Discord.Client) => {
                     }).catch((error) => {
                         console.log(error);
                     });
-
-
-                }else{
+                } else {
                     message.channel.send(regusername);
                     return;
                 }
                 return;
             }
 
+            //Cases for LF
             switch (args[0]) {
                 case "help":
                     let embed = new Discord.RichEmbed()
@@ -119,84 +125,110 @@ module.exports = (bot = Discord.Client) => {
                                 message.reply(response.message);
                                 return Promise.reject(response.message);
                             }
+                            if (!response.data.recenttracks) {
+                                console.log(`No Recent`);
+                                return;
+                            }
+                            if (!response.data.recenttracks.track[0]) {
+                                console.log(`No Track`);
+                                return;
+                            }
                             let albumcover = "";
-                            response.data.recenttracks.track[0].image.forEach(image => {
-                                if (image["size"] === "extralarge") {
-                                    albumcover = image["#text"];
-                                }
-                            })
-                           
-                             let album = "";
-                            if (response.data.recenttracks.track[0].album["#text"]){
-                                album =  response.data.recenttracks.track[0].album["#text"];
-                            }else{
+                            if (!response.data.recenttracks.track[0].image) {
+                                console.log(`No Image`);
+                            } else {
+                                response.data.recenttracks.track[0].image.forEach(image => {
+                                    if (image["size"] === "extralarge") {
+                                        albumcover = image["#text"];
+                                    }
+                                })
+
+                            }
+                            let album = "";
+                            if (response.data.recenttracks.track[0].album["#text"]) {
+                                album = response.data.recenttracks.track[0].album["#text"];
+                            } else {
                                 album = "N/A";
-                            } 
+                            }
+                            if (!response.data.recenttracks.track[0]["@attr"]) {
+                                let embed2 = new Discord.RichEmbed()
+                                    .setAuthor(`${username} - No Current Song`, message.author.displayAvatarURL.split("?")[0])
+                                    .setColor("#33cc33")
+                                    .setThumbnail(albumcover)
+                                    .addField("Previous Album", album)
+                                    .addField("Previous Artist", `${response.data.recenttracks.track[0].artist["#text"]}`, true)
+                                    .addField("Previous Song", `[${response.data.recenttracks.track[0].name}](${response.data.recenttracks.track[0].url.replace(/\(/g, "%28").replace(/\)/g, "%29")})`, true)
+                                    .setTimestamp(message.createdAt)
+                                    .setFooter("Powered by last.fm", "https://images-ext-1.discordapp.net/external/EX26VtAQmWawZ6oyRUVaf76Px2JCu0m3iNU6uNv0XE0/https/i.imgur.com/C7u8gqg.jpg");
+                                sendEmbed(message, embed2);
+                                return;
+                            }
 
                             let embed2 = new Discord.RichEmbed()
-                                .setTitle(`${username} - Now Playing`)
+                                .setAuthor(`${username} - Now Playing`, message.author.displayAvatarURL.split("?")[0])
                                 .setColor("#33cc33")
                                 .setThumbnail(albumcover)
                                 .addField("Album", album)
-                                .addField("Arist", response.data.recenttracks.track[0].artist["#text"])
-                                .addField("Song", `[${response.data.recenttracks.track[0].name}](${response.data.recenttracks.track[0].url.replace("(", "%28").replace(")", "%29")})`)  
-                                .addField("Previous Song", `[${response.data.recenttracks.track[1].name}](${response.data.recenttracks.track[1].url.replace("(", "%28").replace(")", "%29")})`)     
+                                .addField("Artist", response.data.recenttracks.track[0].artist["#text"], true)
+                                .addField("Song", `[${response.data.recenttracks.track[0].name}](${response.data.recenttracks.track[0].url.replace(/\(/g, "%28").replace(/\)/g, "%29")})`, true)
+                                .addField("Previous Artist", `${response.data.recenttracks.track[1].artist["#text"]}`, true)
+                                .addField("Previous Song", `[${response.data.recenttracks.track[1].name}](${response.data.recenttracks.track[1].url.replace(/\(/g, "%28").replace(/\)/g, "%29")})`, true)
                                 .setTimestamp(message.createdAt)
-                                .setFooter("Powered by last.fm");
+                                .setFooter("Powered by last.fm", "https://images-ext-1.discordapp.net/external/EX26VtAQmWawZ6oyRUVaf76Px2JCu0m3iNU6uNv0XE0/https/i.imgur.com/C7u8gqg.jpg");
                             sendEmbed(message, embed2);
                             return;
-                    
+
                         }).catch((error) => {
                             console.log(error);
                         });
-                    }else{
+                    } else {
                         message.channel.send(regusername);
                         return;
                     }
                     break;
 
-                    /* case "toptracks":
-                    if (lastfm.has(message.author.id)) {
-                        username = lastfm.get(message.author.id);
+                /* case "toptracks":
+                if (lastfm.has(message.author.id)) {
+                    username = lastfm.get(message.author.id);
 
-                        url3 = `http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${username}&api_key=${bot.botSettings.lastfm}&format=json`;
+                    url3 = `http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${username}&api_key=${bot.botSettings.lastfm}&format=json`;
 
-                        axios.get(url3).then(response => {
-                            if (response.error) {
-                                message.reply(response.message);
-                                return Promise.reject(response.message);
-                            }
-                            
-                            let embed2 = new Discord.RichEmbed()
-                                .setTitle(`${username} - Now Playing`)
-                                .setColor("#33cc33")
-                                .setThumbnail(albumcover)
-                                .addField("Album", response.data.recenttracks.track[0].album["#text"])
-                                .addField("Arist", response.data.recenttracks.track[0].artist["#text"])
-                                .addField("Song", `[${response.data.recenttracks.track[0].name}](${response.data.recenttracks.track[0].url})`)  
-                                .addField("Previous Song", `[${response.data.recenttracks.track[1].name}](${response.data.recenttracks.track[1].url})`)     
-                                .setTimestamp(message.createdAt)
-                                .setFooter("Powered by last.fm");
-                            sendEmbed(message, embed2);
-                            return;
-                    
-                        }).catch((error) => {
-                            console.log(error);
-                        });
-                    }else{
-                        message.channel.send(regusername);
+                    axios.get(url3).then(response => {
+                        if (response.error) {
+                            message.reply(response.message);
+                            return Promise.reject(response.message);
+                        }
+                        
+                        let embed2 = new Discord.RichEmbed()
+                            .setTitle(`${username} - Now Playing`)
+                            .setColor("#33cc33")
+                            .setThumbnail(albumcover)
+                            .addField("Album", response.data.recenttracks.track[0].album["#text"])
+                            .addField("Arist", response.data.recenttracks.track[0].artist["#text"])
+                            .addField("Song", `[${response.data.recenttracks.track[0].name}](${response.data.recenttracks.track[0].url})`)  
+                            .addField("Previous Song", `[${response.data.recenttracks.track[1].name}](${response.data.recenttracks.track[1].url})`)     
+                            .setTimestamp(message.createdAt)
+                            .setFooter("Powered by last.fm");
+                        sendEmbed(message, embed2);
                         return;
-                    }
+                
+                    }).catch((error) => {
+                        console.log(error);
+                    });
+                }else{
+                    message.channel.send(regusername);
+                    return;
+                }
 
-                    break; */
+                break; */
 
                 default:
-                    let embed10 = new Discord.RichEmbed()
+                    let embedDef = new Discord.RichEmbed()
                         .setTitle("LastFM Commands")
                         .setColor("#ffff4d")
                         .setFooter("If you have any other questions please contact Rave#0737");
-                    lastFMHelp(message, prefix, embed2);
-                    sendEmbed(message, embed2);
+                    lastFMHelp(message, prefix, embedDef);
+                    sendEmbed(message, embedDef);
 
                     return;
 
