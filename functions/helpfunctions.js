f9f885bd74a2fff14956a5dbc8dc58a03a74fcdd
Rave <asgarber@live.com>
2018-03-09 15:53:17 -0500

    fixed spacing

--- functions/helpfunctions.js
+++ functions/helpfunctions.js
@@ -3,9 +3,9 @@ const Message = Discord.Message;
 
 module.exports = (bot = Discord.Client) => {
 
-    //Help command for General Commands
-    generalHelp = function generalHelp(message, prefix, embed) {
-        embed.addField(":information_source: Information", `\*\* ${prefix}help\*\* - Shows this list of commands.
+	//Help command for General Commands
+	generalHelp = function generalHelp(message, prefix, embed) {
+		embed.addField(":information_source: Information", `\*\* ${prefix}help\*\* - Shows this list of commands.
 \*\* \\*\\*help\*\* - Will direct message a list of music bot commands.
 \*\* ${prefix}mod\*\* - Shows a list of mod commands.
 \*\* ${prefix}userinfo\*\* - Shows a user's information.
@@ -16,11 +16,11 @@ module.exports = (bot = Discord.Client) => {
 \*\* ${prefix}github\*\* - Sends a link to Lucky Bot's github page.
 \*\* ${prefix}issue\*\* - Please report any issues you are having with Lucky Bot using this command.
 \*\* ${prefix}suggestion\*\* - Have a suggestion for Lucky Bot? Use this command to have it heard!`)
-    }
+	}
 
-    //Help command for Notifications
-    notifyHelp = function notifyHelp(message, prefix, embed) {
-        embed.addField(":round_pushpin: Notificatons", `\*\* ${prefix}notify\*\* - Shows this list of commands for notifications.
+	//Help command for Notifications
+	notifyHelp = function notifyHelp(message, prefix, embed) {
+		embed.addField(":round_pushpin: Notificatons", `\*\* ${prefix}notify\*\* - Shows this list of commands for notifications.
 \*\* ${prefix}notify list\*\* - Direct messages a list of keywords for the server.
 \*\* ${prefix}notify clear\*\* - Removes all keywords for the server.
 \*\* ${prefix}notify add <keyword>\*\* - Adds a <keyword> to notify you about on the server.
@@ -31,106 +31,106 @@ module.exports = (bot = Discord.Client) => {
 \*\* ${prefix}notify global remove <keyword>\*\* - Removes a <keyword> you were notified about on all servers.
 \*\* ${prefix}notify ignore <channel> <#channel>\*\* - Ignores all keyword triggers in <#channel>.
 \*\* ${prefix}notify ignore server\*\* - Ignores all keyword triggers in the server.`)
-    }
+	}
 
-    //Help command for Custom Commands
-    commandsHelp = function commandsHelp(message, prefix, embed) {
-        embed.addField(":speech_left: Custom Commands", `\*\* ${prefix}command\*\* - Shows this list of commands for custom commands.
+	//Help command for Custom Commands
+	commandsHelp = function commandsHelp(message, prefix, embed) {
+		embed.addField(":speech_left: Custom Commands", `\*\* ${prefix}command\*\* - Shows this list of commands for custom commands.
 \*\* ${prefix}command list\*\* - Direct messages a list of custom commands on the server.
 \*\* ${prefix}command add <name> <command>\*\* - Adds a custom command to the server.
 \*\* ${prefix}command remove <name> <command>\*\* - Removes a custom command on the server.
 \*\* ${prefix}command edit <name> <command>\*\* - Edits a custom command on the server.`)
-    }
+	}
 
-    //Help command for Lastfm Commands
-    lastFMHelp = function lastFMHelp(message, prefix, embed) {
-        embed.addField(":musical_note: Lastfm Commands", `\*\* ${prefix}lastfm help\*\* - Shows this list of commands for lastfm commands.
+	//Help command for Lastfm Commands
+	lastFMHelp = function lastFMHelp(message, prefix, embed) {
+		embed.addField(":musical_note: Lastfm Commands", `\*\* ${prefix}lastfm help\*\* - Shows this list of commands for lastfm commands.
 \*\* ${prefix}lastfm\*\* - Shows basic account info.
 \*\* ${prefix}lastfm set\*\* - Saves lastfm username.
 \*\* ${prefix}lastfm nowplaying\*\* - Shows the song currently playing.`)
-    }
+	}
 
-    /* //Help command for Roles
+	/* //Help command for Roles
     rolesHelp = function rolesHelp(message, prefix, embed) {
         embed.addField(":art: Roles", `\*\*+<role> \*\* - Allows user to add the <role>.
 \*\*-<role> \*\* - Allows user to remove the <role>.
 :warning: When adding and removing roles, names must match role name exactly!`)
     } */
 
-    //Mod Help Commands
-    //Help for General Mod Commands
-    modHelpGeneral = function modHelpGeneral(message, prefix, embed) {
-        embed.addField(":exclamation: Basic Commands", `\*\* ${prefix}mod\*\* - Shows this list of commands for mod commands.
+	//Mod Help Commands
+	//Help for General Mod Commands
+	modHelpGeneral = function modHelpGeneral(message, prefix, embed) {
+		embed.addField(":exclamation: Basic Commands", `\*\* ${prefix}mod\*\* - Shows this list of commands for mod commands.
 \*\* ${prefix}setprefix\*\* - Changes the prefix for Lucky Bot.
 \*\* ${prefix}autorole\*\* - Sets a role to be added to a user when they join the server.
 \*\* ${prefix}ban <user> [days] [reason]\*\* - Bans a <user> and removes the messages from [days] for [reason]. Days default is 0.     
 \*\* ${prefix}kick <user> [reason]\*\* - Kicks a <user> for [reason]`)
-    }
+	}
 
-    //Help for Welcome Commands
-    welcomeHelp = function welcomeHelp(message, prefix, embed) {
-        embed.addField("<a:hyperdab:415998364245557259> Welcome Commands", `\*\* ${prefix}welcome\*\* - Shows this list of commands for welcome.
+	//Help for Welcome Commands
+	welcomeHelp = function welcomeHelp(message, prefix, embed) {
+		embed.addField("<a:hyperdab:415998364245557259> Welcome Commands", `\*\* ${prefix}welcome\*\* - Shows this list of commands for welcome.
 \*\* ${prefix}welcome help\*\* - Shows a detailed list of commands for welcome.
 \*\* ${prefix}welcome channel <channel name>\*\* - Sets the channel the bot should welcome new members in.
 \*\* ${prefix}welcome message <message>\*\* - Sets the message the bot says when a new member joins. Use {server} for server name and {user} for the new user. Using {mention} makes the username a mention.`)
-    }
+	}
 
-    //Help for Start Commands
-    startHelp = function startHelp(message, prefix, embed) {
-        embed.addField(":checkered_flag: Start Commands", `\*\* ${prefix}start\*\* - Shows this list of commands for start.
+	//Help for Start Commands
+	startHelp = function startHelp(message, prefix, embed) {
+		embed.addField(":checkered_flag: Start Commands", `\*\* ${prefix}start\*\* - Shows this list of commands for start.
 \*\* ${prefix}start help\*\* - Shows a detailed list of commands for start.
 \*\* ${prefix}start roles <channel name>\*\* - Sets the channel for the role system.
 \*\* ${prefix}start logs <channel name>\*\* - Sets the channel for message logs.  
 \*\* ${prefix}start join <channel name>\*\* - Sets the channel for users joining and leaving. (Message cannot be changed on this)`)
-    }
+	}
 
-    //Help for Toggle Commands
-    toggleHelp = function toggleHelp(message, prefix, embed) {
-        embed.addField(":arrows_counterclockwise: Toggle Commands", `\*\* ${prefix}toggle\*\* - Shows this list of commands for toggles.
+	//Help for Toggle Commands
+	toggleHelp = function toggleHelp(message, prefix, embed) {
+		embed.addField(":arrows_counterclockwise: Toggle Commands", `\*\* ${prefix}toggle\*\* - Shows this list of commands for toggles.
 \*\* ${prefix}toggle image\*\* - Changes between embed disabled for images in message logs.
 \*\* ${prefix}toggle logs\*\* - Turns message logs on and off.
 \*\* ${prefix}toggle welcome\*\* - Turns welcome messages on and off.
 \*\* ${prefix}toggle roles\*\* - Turns roles channel on and off.`);
-    }
+	}
 
-    //Help for Server Commands
-    ownerServerHelp = function ownerServerHelp(message, prefix, embed) {
-        embed.addField(":speech_left: Sever Commands", `\*\* ${prefix}server\*\* - Shows this list of commands for server.
+	//Help for Server Commands
+	ownerServerHelp = function ownerServerHelp(message, prefix, embed) {
+		embed.addField(":speech_left: Sever Commands", `\*\* ${prefix}server\*\* - Shows this list of commands for server.
 \*\* ${prefix}server list\*\* - Shows the servers Lucky Bot is in.
 \*\* ${prefix}server leave\*\* - Allows Lucky Bot to leave a server it is in.
 \*\* ${prefix}server settings <serverid>\*\* - Shows enabled settings on <server>. Defaults to current server if no id is provided.`)
-    }
+	}
 
-    //Help for Relay Commands
-    relayHelp = function relayHelp(message, prefix, embed) {
-        embed.addField(":arrows_counterclockwise: Relay", `\*\* ${prefix}relay\*\* - Shows this list of commands for relay.
+	//Help for Relay Commands
+	relayHelp = function relayHelp(message, prefix, embed) {
+		embed.addField(":arrows_counterclockwise: Relay", `\*\* ${prefix}relay\*\* - Shows this list of commands for relay.
 \*\* ${prefix}relay list\*\* - Shows existing relays.
 \*\* ${prefix}relay start <name> <type> <chanID1> <chanID2>\*\* - Starts a relay of <type> between at least two channels.
 \*\* ${prefix}relay add <name> <chanID>\*\* - Adds a channel to an existing relay.
 \*\* ${prefix}relay remove <name> <chanID>\*\* - Removes a channel from an existing relay.
 \*\* ${prefix}relay delete <name>\*\* - Deletes an existing relay.`)
-    }
+	}
 
-    relayHelp2 = function relayHelp2(message, embed){
-        embed.addField(":arrows_counterclockwise: Relay", `Relays connect a two way chat system between two channels on a server or between servers. Please contact Rave#0737 or OrigamiCoder#1375 to have one set up.`)
-    }
+	relayHelp2 = function relayHelp2(message, embed){
+		embed.addField(":arrows_counterclockwise: Relay", `Relays connect a two way chat system between two channels on a server or between servers. Please contact Rave#0737 or OrigamiCoder#1375 to have one set up.`)
+	}
 
-    //Sends embed in channel if bot has permission otherwise dms
-    sendEmbed = function sendEmbed(message, embed) {
-        let permsOvr = message.channel.permissionsFor(bot.user);
-        if(!permsOvr) {
-            console.log(`No perms returned`);
-            return;
-        }
-        if (!permsOvr.has("EMBED_LINKS")) {
-            message.author.send(embed).catch((error) => {
-                console.log(error);
-            });
-            return;
-        } else {
-            message.channel.send(embed);
-            return;
-        }
-    }
+	//Sends embed in channel if bot has permission otherwise dms
+	sendEmbed = function sendEmbed(message, embed) {
+		let permsOvr = message.channel.permissionsFor(bot.user);
+		if(!permsOvr) {
+			console.log(`No perms returned`);
+			return;
+		}
+		if (!permsOvr.has("EMBED_LINKS")) {
+			message.author.send(embed).catch((error) => {
+				console.log(error);
+			});
+			return;
+		} else {
+			message.channel.send(embed);
+			return;
+		}
+	}
 
 }
--- modules/customcommands.js
+++ modules/customcommands.js
@@ -4,177 +4,177 @@ let commandnames = customcmds.commands;
 
 module.exports = (bot = Discord.Client) => {
 
-    require("./../functions/helpfunctions.js")(bot);
+	require("./../functions/helpfunctions.js")(bot);
 
-    const Enmap = require("enmap");
-    const EnmapLevel = require("enmap-level");
-    const commandProvider = new EnmapLevel({ name: 'Commands' });
-    cmds = new Enmap({ provider: commandProvider });
+	const Enmap = require("enmap");
+	const EnmapLevel = require("enmap-level");
+	const commandProvider = new EnmapLevel({ name: 'Commands' });
+	cmds = new Enmap({ provider: commandProvider });
 
-    customCommands = async function customCommands(message) {
-        if (message.system) return;
-        if (message.author.bot) return;
-        if (message.channel.type === 'dm') return;
+	customCommands = async function customCommands(message) {
+		if (message.system) return;
+		if (message.author.bot) return;
+		if (message.channel.type === 'dm') return;
 
-        let serverSettings = bot.getServerSettings(message.guild.id);
+		let serverSettings = bot.getServerSettings(message.guild.id);
 
-        let messageArray = message.content.split(" ");
-        let command = messageArray[0];
-        let args = messageArray.slice(1);
-        const prefix = serverSettings.prefix;
+		let messageArray = message.content.split(" ");
+		let command = messageArray[0];
+		let args = messageArray.slice(1);
+		const prefix = serverSettings.prefix;
 
-        if (!command.startsWith(prefix)) return;
+		if (!command.startsWith(prefix)) return;
 
-        const guild = message.guild;
+		const guild = message.guild;
 
-        if (command === `${prefix}command`) {
-            console.log("Crash at command");
-            if (args.length === 0) {
-                let embed = new Discord.RichEmbed()
-                    .setTitle("Custom Command Help")
-                    .setColor("#40e0d0")
-                    .setFooter("If you have any other questions please contact Rave#0737");
-                commandsHelp(message, prefix, embed);
-                message.channel.send(embed);
-                return;
-            }
+		if (command === `${prefix}command`) {
+			console.log("Crash at command");
+			if (args.length === 0) {
+				let embed = new Discord.RichEmbed()
+					.setTitle("Custom Command Help")
+					.setColor("#40e0d0")
+					.setFooter("If you have any other questions please contact Rave#0737");
+				commandsHelp(message, prefix, embed);
+				message.channel.send(embed);
+				return;
+			}
 
-            let custom = null;
-            let cmdName = "";
+			let custom = null;
+			let cmdName = "";
+			let embed;
+			let list = [];
 
-            switch (args[0]) {
+			switch (args[0]) {
 
-                case "add":
-                    console.log("Crash at cmd add");
-                    if (args.length < 3) {
-                        message.channel.send("**Please add a command name and the command.**");
-                        return;
-                    }
+				case "add":
+					console.log("Crash at cmd add");
+					if (args.length < 3) {
+						message.channel.send("**Please add a command name and the command.**");
+						return;
+					}
 
-                    if (cmds.has(guild.id)) {
-                        custom = cmds.get(guild.id);
-                    }
+					if (cmds.has(guild.id)) {
+						custom = cmds.get(guild.id);
+					}
 
-                    if (!custom) {
-                        custom = {};
-                    }
+					if (!custom) {
+						custom = {};
+					}
 
-                    cmdName = args[1].toLowerCase();
+					cmdName = args[1].toLowerCase();
 
-                    if (commandnames.includes(cmdName)) {
-                        message.channel.send("**That is a standard command try another name.**");
-                        return;
-                    }
+					if (commandnames.includes(cmdName)) {
+						message.channel.send("**That is a standard command try another name.**");
+						return;
+					}
 
-                    if (custom[cmdName]) {
-                        message.channel.send("**That command already exists. Please use \`edit\` to edit the command.**");
-                        return;
-                    }
+					if (custom[cmdName]) {
+						message.channel.send("**That command already exists. Please use `edit` to edit the command.**");
+						return;
+					}
 
-                    custom[cmdName] = args.slice(2).join(" ");
-                    cmds.set(guild.id, custom);
-                    message.channel.send("**Custom command added.**");
-                    break;
+					custom[cmdName] = args.slice(2).join(" ");
+					cmds.set(guild.id, custom);
+					message.channel.send("**Custom command added.**");
+					break;
 
-                case "edit":
-                    console.log("Crash at cmd edit");
-                    if (args.length < 3) {
-                        message.channel.send("**Please add a command name and the command.**");
-                        return;
-                    }
+				case "edit":
+					console.log("Crash at cmd edit");
+					if (args.length < 3) {
+						message.channel.send("**Please add a command name and the command.**");
+						return;
+					}
 
-                    custom = cmds.get(guild.id);
+					custom = cmds.get(guild.id);
 
-                    if (!custom) {
-                        message.channel.send("**There are no custom commands on the server.**");
-                        return;
-                    }
+					if (!custom) {
+						message.channel.send("**There are no custom commands on the server.**");
+						return;
+					}
 
-                    cmdName = args[1].toLowerCase();
+					cmdName = args[1].toLowerCase();
 
-                    if (!custom[cmdName]) {
-                        message.channel.send("**That command does not exist.**");
-                        return;
-                    }
+					if (!custom[cmdName]) {
+						message.channel.send("**That command does not exist.**");
+						return;
+					}
 
-                    if (!custom.hasOwnProperty(cmdName)) {
-                        return;
-                    }
+					if (!custom.hasOwnProperty(cmdName)) {
+						return;
+					}
 
-                    custom[cmdName] = args.slice(2).join(" ");
-                    cmds.set(guild.id, custom);
-                    message.channel.send("**Custom command edited.**");
-                    break;
+					custom[cmdName] = args.slice(2).join(" ");
+					cmds.set(guild.id, custom);
+					message.channel.send("**Custom command edited.**");
+					break;
 
-                case "remove":
-                    console.log("Crash at cmd remove");
-                    if (args.length < 2) {
-                        message.channel.send("**Please add a command name to remove.**");
-                        return;
-                    }
+				case "remove":
+					console.log("Crash at cmd remove");
+					if (args.length < 2) {
+						message.channel.send("**Please add a command name to remove.**");
+						return;
+					}
 
-                    custom = cmds.get(guild.id);
+					custom = cmds.get(guild.id);
 
-                    if (!custom) {
-                        message.channel.send("**There are no custom commands on the server.**");
-                        return;
-                    }
+					if (!custom) {
+						message.channel.send("**There are no custom commands on the server.**");
+						return;
+					}
 
-                    cmdName = args[1].toLowerCase();
+					cmdName = args[1].toLowerCase();
 
-                    if (!custom[cmdName]) {
-                        message.channel.send("**That command does not exist.**");
-                        return;
-                    }
+					if (!custom[cmdName]) {
+						message.channel.send("**That command does not exist.**");
+						return;
+					}
 
-                    if (!custom.hasOwnProperty(cmdName)) {
-                        return;
-                    }
+					if (!custom.hasOwnProperty(cmdName)) {
+						return;
+					}
 
-                    delete custom[cmdName];
-                    cmds.set(guild.id, custom);
-                    message.channel.send("**Custom command removed.**");
-                    break;
+					delete custom[cmdName];
+					cmds.set(guild.id, custom);
+					message.channel.send("**Custom command removed.**");
+					break;
 
-                case "list":
-                    console.log("Crash at cmd list");
-                    if (cmds.has(guild.id)) {
-                        custom = cmds.get(guild.id);
-                    }
+				case "list":
+					console.log("Crash at cmd list");
+					if (cmds.has(guild.id)) {
+						custom = cmds.get(guild.id);
+					}
 
-                    if (!custom) {
-                        message.channel.send("**There are no custom commands on this server.**");
-                        return;
-                    }
+					if (!custom) {
+						message.channel.send("**There are no custom commands on this server.**");
+						return;
+					}
 
-                    let list = [];
-                    for (var key in custom) {
-                        if (custom.hasOwnProperty(key)) {
-                            list.push(`${prefix}${key}`);
-                        }
-                    }
+					for (var key in custom) {
+						if (custom.hasOwnProperty(key)) {
+							list.push(`${prefix}${key}`);
+						}
+					}
 
-                    if (list.length === 0) {
-                        message.channel.send("**There are no custom commands on this server.**");
-                        return;
-                    }
+					if (list.length === 0) {
+						message.channel.send("**There are no custom commands on this server.**");
+						return;
+					}
 
-                    message.channel.send("**List of commands sent to direct messages.**");
-                    message.author.send(`\`\`\`List of custom commands for ${message.guild.name}:
+					message.channel.send("**List of commands sent to direct messages.**");
+					message.author.send(`\`\`\`List of custom commands for ${message.guild.name}:
 ${list.join(`\n`)}\`\`\``);
-                    break;
+					break;
 
-                default:
-                        let embed;
-                        embed.setTitle("Custom Command Help")
-                        .setColor("#40e0d0")
-                        .setFooter("If you have any other questions please contact Rave#0737");
-                    commandsHelp(message, prefix, embed);
-                    message.channel.send(embed);
-                    return;
-            }
+				default:
+					embed.setTitle("Custom Command Help")
+						.setColor("#40e0d0")
+						.setFooter("If you have any other questions please contact Rave#0737");
+					commandsHelp(message, prefix, embed);
+					message.channel.send(embed);
+					return;
+			}
 
-        }
-    };
+		}
+	};
 
-}
+};
