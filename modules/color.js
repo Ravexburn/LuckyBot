3dda26054143d2912565d219440a35d40c5f721c
Rave <asgarber@live.com>
2018-04-19 17:54:11 -0400

    added random color option

--- modules/color.js
+++ modules/color.js
@@ -5,15 +5,25 @@ module.exports = (bot = Discord.Client) => {
 	color = function color(message, args) {
 
 		if (args.length === 0) {
-			message.channel.send("Please provide a hex color");
+			message.channel.send("Please provide a hex color or ask for a random color with `color random`");
 			return;
 		}
 
-		let hex = args[0];
-		let color = hex.replace("#", "");
+		let color = args[0].replace("#", "");
+		if (["random", "rand", "r", "ran"].includes(color)) {
+			color = getRandomColor();
+		}
 		let url = `http://www.colourlovers.com/img/${color}/200/200/color.png`;
-		
-		message.reply(`Color \`${hex}\``,{files: [url]});
-	};
+		message.reply(`Color \`${color}\``, { files: [url] });
 
+	};
 };
+
+function getRandomColor() {
+	var letters = '0123456789ABCDEF';
+	var color = "";
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
