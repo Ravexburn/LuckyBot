b8e9305a9f8628e5e2a34e7b9b61d35ecb798324
Rave <asgarber@live.com>
2018-06-06 12:15:22 -0400

    fixes to hyperlink

--- modules/lastfm.js
+++ modules/lastfm.js
@@ -421,7 +421,8 @@ rectrack = function rectrack(message, embed, response) {
 	}
 	let msg = "";
 	for (i = 0; i < responseA.length; i++) {
-		msg += `${i + 1}. [${responseA[i].name}](${responseA[i].url.replace(/\(/g, "%28").replace(/\)/g, "%29")}) by [${responseA[i].artist["#text"]}](https://www.last.fm/music/${responseA[i].artist["#text"].replace(" ", "+")}) \n`;
+		msg += `${i + 1}. [${responseA[i].name}](${responseA[i].url.replace(/\(/g, "%28").replace(/\)/g, "%29")}) `;
+		msg += `by [${responseA[i].artist["#text"]}](https://www.last.fm/music/${responseA[i].artist["#text"].replace(" ", "+").replace(/\(/g, "%28").replace(/\)/g, "%29")}) \n`;
 	}
 	embedcss(message, embed, msg);
 };
