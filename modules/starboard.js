a0b704d55f6e1b297b24cbf0be54a898dfe141ae
Rave <asgarber@live.com>
2018-06-16 17:24:43 -0400

    syntax

--- modules/starboard.js
+++ modules/starboard.js
@@ -2,53 +2,53 @@ const Discord = require("discord.js");
 
 module.exports = (bot = Discord.Client) => {
 
-    //Add reaction listener for starboard
+	//Add reaction listener for starboard
 
-    starboardListen = function starboardListen(message) {
+	starboardListen = function starboardListen(message) {
 		const guild = message.guild;
-        const serverSettings = bot.getServerSettings(guild.id);
-        
-        if (!serverSettings.starboardOn || serverSettings.starboardChannelID === "") return;
+		const serverSettings = bot.getServerSettings(guild.id);
 
-        const starboardEmoji = serverSettings.starboardEmoji;
+		if (!serverSettings.starboardOn || serverSettings.starboardChannelID === "") return;
 
-        const listener = message.createReactionCollector(r => r.emoji.name == serverSettings.starboardEmoji, { time: 600000 });
-        listener.on('collect', reaction => {
-            if (listener.collected.first().count == serverSettings.starboardNumber) {
-                starboardPost(message);
-            }
-        });
-    }
+		const starboardEmoji = serverSettings.starboardEmoji;
 
-    //Post message to starboard
+		const listener = message.createReactionCollector(r => r.emoji.name == serverSettings.starboardEmoji, { time: 600000 });
+		listener.on('collect', reaction => {
+			if (listener.collected.first().count == serverSettings.starboardNumber) {
+				starboardPost(message);
+			}
+		});
+	};
 
-    starboardPost = function starboardPost(message) {
+	//Post message to starboard
+
+	starboardPost = function starboardPost(message) {
 		const guild = message.guild;
 		const serverSettings = bot.getServerSettings(guild.id);
 		if (!serverSettings) return;
 		if (!serverSettings.starboardChannelID) return;
-        const starboardChannelID = serverSettings.starboardChannelID;
+		const starboardChannelID = serverSettings.starboardChannelID;
 
 		if (!guild.channels.has(starboardChannelID)) {
 			return;
 		}
 		const boardChannel = guild.channels.get(starboardChannelID);
-        if (serverSettings.starboardOn === false) return;
-        
-        let channel = message.channel;
-        if (channel == boardChannel) return;
+		if (serverSettings.starboardOn === false) return;
 
-        let author = message.author.username;
-        let image = message.author.displayAvatarURL;
-        let timestamp = message.createdAt;
-        let msg = message.content;
+		let channel = message.channel;
+		if (channel == boardChannel) return;
 
-        let embed = new Discord.RichEmbed()
-            .setColor("RANDOM")
-            .setThumbnail(image)
-            .setTitle(author)
+		let author = message.author.username;
+		let image = message.author.displayAvatarURL;
+		let timestamp = message.createdAt;
+		let msg = message.content;
+
+		let embed = new Discord.RichEmbed()
+			.setColor("RANDOM")
+			.setThumbnail(image)
+			.setTitle(author)
 			.setFooter("#" + channel.name)
 			.setDescription(msg)
-        boardChannel.send(embed);
-    }
-}
+		boardChannel.send(embed);
+	};
+};
