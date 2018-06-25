d89845c076e855de5d8cde2d5648567382c277ab
Rave <asgarber@live.com>
2018-06-25 15:52:21 -0400

    fixed rolelist

--- modules/rolelist.js
+++ modules/rolelist.js
@@ -6,15 +6,15 @@ module.exports = (bot = Discord.Client) => {
 
 		require("./../functions/helpfunctions.js")(bot);
 
-		let roles = message.guild.roles.array().sort((a,b) => b.position - a.position).map(x => `${x.name} (#${x.id}, #${x.hexColor}) \`# of members: ${x.members.size}\``);
-		
+		let roles = message.guild.roles.array().sort((a,b) => b.position - a.position).map(x => `${x.name} (\`#${x.id}\`, \`${x.hexColor}\`) \`# of members: ${x.members.size}\``);
 
 		var pages = toEmbedPages(roles);
+		
+		console.log(roles);
 
 		let embed = new Discord.RichEmbed()
 			.setColor("#FFAEB9")
-			.setAuthor(`Roles for ${message.guild.name}`)
-			.setDescription(roles);
+			.setAuthor(`Roles for ${message.guild.name}`);
 		embedPages(message, embed, pages);
 		return;
 	};
